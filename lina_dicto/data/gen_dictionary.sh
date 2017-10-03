62c0c07a0436f53a4f7da302f8e1cd550b30f17e
Michinari Nukazawa <michinari.nukazawa@gmail.com>
2017-10-03 23:51:06 +0900

    dictionary converter bash script

--- lina_dicto/data/gen_dictionary.sh
+++ lina_dicto/data/gen_dictionary.sh
@@ -11,10 +11,18 @@ trap 'echo "error:$0($LINENO) \"$BASH_COMMAND\" \"$@\""' ERR
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
 INPUT_FILE=""
+OUTPUT_FILE=""
 
 function convert_utf(){
 	OUTPUT_FILE=${INPUT_FILE%.*}_utf8.txt
 	iconv -f Shift-JIS -t UTF8 "${INPUT_FILE}" |  perl -p -e 's/\r\n/\n/' > "${OUTPUT_FILE}"
+}
+
+function convert_json(){
+	echo -e "[" > "${OUTPUT_FILE}"
+	cat "${INPUT_FILE}" | perl -p -e 's/^([^:]+):(.*)$/["\1","\2"],/' >> "${OUTPUT_FILE}"
+	sed -i '$s/.$//' "${OUTPUT_FILE}"	# 末尾の','を除去する
+	echo -e "]" >> "${OUTPUT_FILE}"
 }
 
 mkdir -p w_dict/
@@ -30,6 +38,11 @@ convert_utf
 rm ${INPUT_FILE}
 INPUT_FILE="pejvo.txt"
 convert_utf
+rm ${INPUT_FILE}
+
+INPUT_FILE="pejvo_utf8.txt"
+OUTPUT_FILE="../dictionary00.json"
+convert_json
 rm ${INPUT_FILE}
 
 popd
