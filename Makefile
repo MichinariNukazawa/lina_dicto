cfd1dd02ce9fc4051f336933a98958e1611fe9be
Michinari Nukazawa <michinari.nukazawa@gmail.com>
2017-11-24 08:28:22 +0900

    debian package

--- Makefile
+++ Makefile
@@ -17,4 +17,5 @@ package: package_desktop
 package_desktop:
 	bash ./packaging_win64.sh
 	bash ./packaging_macosx.sh
+	cd lina_dicto && npm run installer_debian_amd64
 
---
+++ lina_dicto/build_default.js
@@ -0,0 +1,31 @@
+const config = require('./package.json');
+
+// process.on('unhandledRejection', console.dir);
+
+module.exports =
+{
+	dir: './',
+	out: '../release',
+
+	'app-bundle-id': 'com.michinari_nukazawa/lina_dicto',
+
+	'app-version': config.version,
+
+	overwrite: true,  // 上書き
+
+	'helper-bundle-id': 'com.michinari_nukazawa/lina_dicto',
+	overwrite: true,
+	asar: true,
+	prune: true,
+	ignore: "node_modules/(electron-packager|electron-prebuilt|\.bin)|release\.js",
+	'version-string': {
+		CompanyName: 'daisy_bell',
+		FileDescription: 'Dictionary Application, Japanese to Esperanto',
+		OriginalFilename: config.name,
+		FileVersion: config.version,
+		ProductVersion: config.version,
+		ProductName: config.name,
+		InternalName: config.name
+	}
+};
+
