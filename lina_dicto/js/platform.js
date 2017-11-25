13df80dabf03e5845c0be0e4094929c78f98d1a8
Michinari Nukazawa <michinari.nukazawa@gmail.com>
2017-11-26 00:06:43 +0900

    add context menu (light click menu) copy, paste

--- lina_dicto/js/platform.js
+++ lina_dicto/js/platform.js
@@ -1,10 +1,32 @@
 'use strict';
+/** 環境依存の機能をここに書く(非electron環境で機能拡張する等) */
 
+// const remote = require('electron').remote;
+// const Menu = remote.Menu;
 
-/** 非electron環境で機能拡張等するための機構なので、electronターゲットの本環境では何もしない */
 class Platform{
 	init()
 	{
+
+		//! context menu (light click menu)
+		var menu = new Menu();
+
+		menu.append(new MenuItem({
+			label: 'Copy',
+			accelerator: 'CmdOrCtrl+C',
+			role: 'copy'
+		}));
+		menu.append(new MenuItem({
+			label: 'Paste',
+			accelerator: 'CmdOrCtrl+V',
+			role: 'paste'
+		}));
+
+		window.addEventListener('contextmenu', function (e) {
+			e.preventDefault();
+			menu.popup(remote.getCurrentWindow());
+		}, false);
+
 		return true;
 	}
 
