036aba0415ecdd5d8e753eec9740c6b6c8141059
MichinariNukazawa <michinari.nukazawa@gmail.com>
2018-11-03 17:37:19 +0900

    Dictionary methods timeout test

--- lina_dicto/test/timeout.js
+++ lina_dicto/test/timeout.js
@@ -54,8 +54,9 @@ it ("Dictionary.get_item_from_keyword timeout", function(done) {
 	done();
 }).timeout(1500);
 
-
 it ("Dictionary.get_index_from_incremental_keyword timeout", function(done) {
+	this.retries(2);
+
 	const EO_TO_JA_DICT_DATAS = [
 		['voc^o',		{'isMatch':true}],
 		['bonan',		{'isMatch':true}],
@@ -86,7 +87,69 @@ it ("Dictionary.get_index_from_incremental_keyword timeout", function(done) {
 
 	setTimeout(done, 100); // 実行完了が早すぎる場合に実行時間が表示されない
 
-}).timeout(200);
+}).timeout(200); // 基本的に200で十分なのだけれど、たまにハズレ値を出す
+
+it ("Dictionary.get_indexes_from_jakeyword timeout", function(done) {
+	this.retries(2);
+
+	const JA_TO_EO_DICT_DATAS = [
+		['そろばん',		{'matchs':['abak/o', 'glob/kalkul/il/o', 'japan/a abak/o']}],
+		['気づかい',		{'matchs':['zorgo']}],
+		['心配',		{'matchs':['maltrankvilo', 'timo', 'zorgo']}],
+		['あああああ',		{'matchs':[]}],
+		['ををををを',		{'matchs':[]}],
+		['ーーーーー',		{'matchs':[]}],
+		['あーーーーー',	{'matchs':[]}],
+		['植物',		{'matchs':['kresk/aj^/o', 'plant/ar/o', 'plant/o', 'veget/aj^/o', 'vegetal/o']}],
+	];
+
+	const datas = JA_TO_EO_DICT_DATAS;
+	for(let c = 0; c < 10; c++){
+		for(let i = 0; i < datas.length; i++){
+			const data = datas[i];
+			const indexes = Dictionary.get_indexes_from_jakeyword(dictionary_handle, data[0]);
+			assert(indexes.length === data[1].matchs.length);
+			for(let t = 0; t < data[1].matchs.length; t++){
+				const item = Dictionary.get_item_from_index(dictionary_handle, indexes[t]);
+				const show_word = Dictionary.get_show_word_from_item(dictionary_handle, item);
+				//console.log('## jkey' ,i, t, data[1].matchs[t].replace(/\//g, ''), show_word);
+				assert(data[1].matchs[t].replace(/\//g, '') === show_word);
+			}
+		}
+	}
+
+	done();
+}).timeout(2000);
+
+it ("Dictionary.get_glosses_info_from_jakeyword timeout", function(done) {
+	this.retries(2);
+
+	const JA_TO_EO_DICT_DATAS = [
+		['そろばん',		{'count': 1, 'matchs':['abak/o', 'glob/kalkul/il/o', 'japan/a abak/o']}],
+		['気づかい',		{'count': 1, 'matchs':['zorgo']}],
+		['心配',		{'count': 3, 'matchs':['maltrankvilo', 'timo', 'zorgo']}],
+		['あああああ',		{'count': 0, 'matchs':[]}],
+		['ををををを',		{'count': 0, 'matchs':[]}],
+		['ーーーーー',		{'count': 0, 'matchs':[]}],
+		['あーーーーー',	{'count': 0, 'matchs':[]}],
+		['植物',		{'count': 3, 'matchs':['kresk/aj^/o', 'plant/ar/o', 'plant/o', 'veget/aj^/o', 'vegetal/o']}],
+		['恋',			{'count': 3}],
+		['愛',			{'count': 3}],
+		['恋愛',		{'count': 3}],
+	];
+
+	const datas = JA_TO_EO_DICT_DATAS;
+	for(let c = 0; c < 10; c++){
+		for(let i = 0; i < datas.length; i++){
+			const data = datas[i];
+			const glosses = Dictionary.get_glosses_info_from_jakeyword(dictionary_handle, data[0]);
+			// console.log('##jg', glosses.length, data[1].count, data[0]);
+			assert(glosses.length === data[1].count);
+		}
+	}
+
+	done();
+}).timeout(2000);
 
 it ("Linad.getResponsesFromKeystring timeout", function(done) {
 
