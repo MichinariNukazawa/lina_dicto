3936b5a94df72dd47d4985fbdd9044ecde554565
Michinari Nukazawa <michinari.nukazawa@gmail.com>
2019-09-23 04:26:48 +0900

    fix cut,copy not working on macosx

--- lina_dicto/js/menu.js
+++ lina_dicto/js/menu.js
@@ -80,8 +80,18 @@ var template = [
 {
 	label: '&Edit',
 	submenu: [
+	// キーボード・ショートカット表示用のダミー(js/index.js onloadにて処理)
 	{
-		// キーボード・ショートカット表示用のダミー(js/index.js onloadにて処理)
+		label: '&Cut',
+		accelerator: 'CmdOrCtrl+X',
+		selector: "cut:"
+	},
+	{
+		label: '&Copy',
+		accelerator: 'CmdOrCtrl+C',
+		selector: "copy:"
+	},
+	{
 		label: '&Paste',
 		accelerator: 'CmdOrCtrl+V',
 		selector: "paste:"
