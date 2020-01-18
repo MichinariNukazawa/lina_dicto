e4ed9f396f33ab763d87b63a0ef9b020d29e85cf
Michinari Nukazawa <michinari.nukazawa@gmail.com>
2020-01-18 10:53:34 +0900

    timeout test add data

--- lina_dicto/test/timeout.js
+++ lina_dicto/test/timeout.js
@@ -32,10 +32,29 @@ it ("Dictionary.query_item_from_keyword timeout", function(done) {
 		['vitra',		{'isMatch':true}],
 		['zorgo',		{'isMatch':true}],
 		['boc^o',		{'isMatch':false}],
-		['zzz',			{'isMatch':false}],
-		['xxxxxxxxxx',		{'isMatch':false}],
 		['BBBBBB',		{'isMatch':false}],
 		['BONAN matenon',	{'isMatch':true}],
+		['vocxa',		{'isMatch':false}],
+		['vocxo',		{'isMatch':false}],
+		['bocxa',		{'isMatch':false}],
+		['boc^o',		{'isMatch':false}],
+		['boc^a',		{'isMatch':false}],
+		['boc^o',		{'isMatch':false}],
+		['eee',			{'isMatch':false}],
+		['sss',			{'isMatch':false}],
+		['zzz',			{'isMatch':false}],
+		['eeeee',		{'isMatch':false}],
+		['sssss',		{'isMatch':false}],
+		['zzzzz',		{'isMatch':false}],
+		['eeeeee',		{'isMatch':false}],
+		['ssssss',		{'isMatch':false}],
+		['zzzzzz',		{'isMatch':false}],
+		['eeeeeee',		{'isMatch':false}],
+		['sssssss',		{'isMatch':false}],
+		['zzzzzzz',		{'isMatch':false}],
+		['eeeeeeeeee',		{'isMatch':false}],
+		['ssssssssss',		{'isMatch':false}],
+		['xxxxxxxxxx',		{'isMatch':false}],
 	];
 
 	const datas = EO_TO_JA_DICT_DATAS;
@@ -53,8 +72,8 @@ it ("Dictionary.query_item_from_keyword timeout", function(done) {
 		}
 	}
 
-	done();
-}).timeout(1500);
+	setTimeout(done, 100); // 実行完了が早すぎる場合に実行時間が表示されない
+}).timeout(200);
 
 it ("Dictionary.query_index_from_incremental_keyword timeout", function(done) {
 	this.retries(2);
@@ -74,6 +93,20 @@ it ("Dictionary.query_index_from_incremental_keyword timeout", function(done) {
 		['zorgo',		{'isMatch':true}],
 		['voc^a',		{'isMatch':true}],
 		['voc^o',		{'isMatch':true}],
+		['bon',			{'isMatch':true}],
+		['bona',		{'isMatch':true}],
+		['bonan mateno',	{'isMatch':true}],
+		['bonan nokto',		{'isMatch':true}],
+		['kaf',			{'isMatch':true}],
+		['kafber',		{'isMatch':true}],
+		['kafolakt',		{'isMatch':true}],
+		['kafplantej',		{'isMatch':true}],
+		['vitr',		{'isMatch':true}],
+		['vitr',		{'isMatch':true}],
+		['zorg',		{'isMatch':true}],
+		['zorg',		{'isMatch':true}],
+		['voc^',		{'isMatch':true}],
+		['voc^',		{'isMatch':true}],
 		['vocxa',		{'isMatch':false}],
 		['vocxo',		{'isMatch':false}],
 		['bocxa',		{'isMatch':false}],
@@ -113,7 +146,6 @@ it ("Dictionary.query_index_from_incremental_keyword timeout", function(done) {
 	}
 
 	setTimeout(done, 100); // 実行完了が早すぎる場合に実行時間が表示されない
-
 }).timeout(200);
 
 it ("Dictionary.query_indexes_from_jakeyword timeout", function(done) {
