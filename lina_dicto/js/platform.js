042f3ddcfd31a32fb935d6270f4d40b84034bb8b
MichinariNukazawa <michinari.nukazawa@gmail.com>
2020-06-14 17:57:11 +0900

    context menu google search, translate from right click selected text

--- lina_dicto/js/platform.js
+++ lina_dicto/js/platform.js
@@ -21,6 +21,38 @@ module.exports = class Platform{
 			accelerator: 'CmdOrCtrl+V',
 			role: 'paste'
 		}));
+		menu.append(new MenuItem({
+			label: 'goto google search(external browser)',
+			click: function(){
+				const keyword = window.getSelection().toString();
+				if(0 == keyword){
+					alert("text no selected");
+					return;
+				}
+				const link = 'https://www.google.com/search?q=' + keyword.replace(/\s+/, '+');
+				//const src_lang =;
+				//const dst_lang = '';
+				//const link = ExternalBrowser.get_google_translate_url(
+				//		keyword, src_lang, dst_lang);
+				ExternalBrowser.open(link);
+			}
+		}));
+		menu.append(new MenuItem({
+			label: 'goto google translate(external browser)',
+			click: function(){
+				const keyword = window.getSelection().toString();
+				if(0 == keyword){
+					alert("text no selected");
+					return;
+				}
+				const isEsp = Esperanto.is_esperanto_string(keyword);
+				const src_lang = (isEsp ? Language.get_code_by_google() : 'ja');
+				const dst_lang = ((! isEsp) ? Language.get_code_by_google() : 'ja');
+				const link = ExternalBrowser.get_google_translate_url(
+						keyword, src_lang, dst_lang);
+				ExternalBrowser.open(link);
+			}
+		}));
 
 		window.addEventListener('contextmenu', function (e) {
 			e.preventDefault();
